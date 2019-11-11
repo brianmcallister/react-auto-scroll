65113c22c8004e030bb193bb9c8023154e97a8df
Brian McAllister <brian@brianmcallister.com>
2019-11-10 23:09:02 -0500

    Initial commit.

---
+++ dist/AutoScroll.d.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+interface Props {
+    checkBoxId?: string;
+    children: React.ReactNode;
+    className?: string;
+    height?: number;
+    optionText?: string;
+    preventInteraction?: boolean;
+    showOption?: boolean;
+}
+declare const _default: ({ checkBoxId, children, className, height, optionText, preventInteraction, showOption, }: Props) => JSX.Element;
+/**
+ * AutoScroll component.
+ */
+export default _default;
