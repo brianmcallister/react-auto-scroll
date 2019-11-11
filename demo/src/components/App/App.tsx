5c998452fd4b98dd67a965edf29b86f55339ec38
Brian McAllister <brian@brianmcallister.com>
2019-11-11 11:00:05 -0500

    Wire up state for controls.

--- demo/src/components/App/App.tsx
+++ demo/src/components/App/App.tsx
@@ -6,64 +6,96 @@ import LogoIcon from '../LogoIcon';
 
 import './_app.scss';
 
+const DEFAULT_HEIGHT = 300;
 const baseClass = 'app';
 
 /**
  * App component.
  */
-export default () => (
-  <div className={baseClass}>
-    <header className={`${baseClass}__header`}>
-      <LogoIcon />
+export default () => {
+  const [preventInteraction, setPreventInteraction] = React.useState(false);
+  const [autoScroll, setAutoScroll] = React.useState(true);
+  const [optionText, setOptionText] = React.useState('Auto scroll');
+  const [height, setHeight] = React.useState(DEFAULT_HEIGHT);
 
-      <a className={`${baseClass}__header-link`} href="https://www.brianmcallister.com">
-        Brian Wm. McAllister
-      </a>
+  return (
+    <div className={baseClass}>
+      <header className={`${baseClass}__header`}>
+        <LogoIcon />
 
-      <div className={`${baseClass}__header-links`}>
-        <a href="https://github.com/brianmcallister/react-highlight-text">GitHub</a>
-        <a href="https://npmjs.com/package/@brianmcallister/react-highlight-text">npm</a>
-      </div>
-    </header>
+        <a className={`${baseClass}__header-link`} href="https://www.brianmcallister.com">
+          Brian Wm. McAllister
+        </a>
 
-    <div className={`${baseClass}__content`}>
-      hello
+        <div className={`${baseClass}__header-links`}>
+          <a href="https://github.com/brianmcallister/react-highlight-text">GitHub</a>
+          <a href="https://npmjs.com/package/@brianmcallister/react-highlight-text">npm</a>
+        </div>
+      </header>
 
-      <div className={`${baseClass}__options`}>
-        <div>
+      <div className={`${baseClass}__content`}>
+        <div className={`${baseClass}__options`}>
+          <div className={`${baseClass}__option-group`}>
+            <label>
+              <input
+                onChange={() => setPreventInteraction(!preventInteraction)}
+                type="checkbox"
+                checked={preventInteraction}
+              />
 
+              Prevent interaction
+            </label>
 
-          <label>
-            Prevent interaction
-            <input type="checkbox" />
-          </label>
+            <label>
+              <input
+                onChange={() => setAutoScroll(!autoScroll)}
+                type="checkbox"
+                checked={autoScroll}
+              />
 
+              Show auto scroll option
+            </label>
+          </div>
+
+          <div className={`${baseClass}__option-group`}>
+            <label>
+              <input
+                onChange={(event) => (
+                  setOptionText(event.currentTarget.value)
+                )}
+                type="text"
+                value={optionText}
+              />
+
+              Auto scroll option text
+            </label>
+
+            <label>
+              <input
+                onChange={(event) => (
+                  setHeight(parseInt(event.currentTarget.value, 10))
+                )}
+                min="250"
+                max="400"
+                type="range"
+              />
+
+              Height
+            </label>
+          </div>
         </div>
 
-        <div>
-          <input type="checkbox" />
-
-          <label>Show auto scroll option</label>
+        <div className={`${baseClass}__msg-container`}>
+          <AutoScroll
+            preventInteraction={preventInteraction}
+            showOption={autoScroll}
+            optionText={optionText}
+            height={height}
+          >
+            msg
+          </AutoScroll>
         </div>
-
-        <div>
-          <input type="text" />
-
-          <label>Auto scroll text</label>
-        </div>
-
-        <div>
-          <input type="range" />
-
-          <label>Height</label>
-        </div>
-      </div>
-
-      <div className={`${baseClass}__msg-container`}>
-        <AutoScroll>
-          msg
-        </AutoScroll>
       </div>
     </div>
-  </div>
-);
+  );
+}
